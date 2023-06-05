028d76c2b18afa3d814472062c643c29c9306951
Radosław Pochopień <radek.p.p@wp.pl>
2023-06-05 08:52:31 +0200

    config secure

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$config['bot_token'] = '';
+$config['discord_api_url'] = '';
+
+$config['default_prefix'] = "#";
+$config['bot_invite_url'] = '';
+
+$config['bot_api']['login'] = '';
+$config['bot_api']['password'] = '';
+$config['bot_api']['ip'] = '';
+$config['bot_api']['port'] = '12345';  
+
+ 
+$config['allowed_bot_lang'] = ['pl', 'en'];
+$config['default_bot_lang'] = 'en';
+?>
